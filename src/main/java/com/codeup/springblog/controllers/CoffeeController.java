bc5e353b619288036fe5179ba5f5543b6bdb9c4a
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-11-02 16:51:26 -0400

    views-lec

--- src/main/java/com/codeup/springblog/controllers/CoffeeController.java
+++ src/main/java/com/codeup/springblog/controllers/CoffeeController.java
@@ -1,10 +1,14 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Coffee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class CoffeeController {
@@ -14,12 +18,35 @@ public class CoffeeController {
     }
     @GetMapping("/coffee/{roast}")
     public String roastSelection(@PathVariable String roast, Model model) {
-        model.addAttribute("roast", roast);
-        boolean choseDark = false;
+//        model.addAttribute("roast", roast);
+//        boolean choseDark = false;
+//        if (roast.equals("dark")) {
+//            choseDark = true;
+//        }
+//        model.addAttribute("choseDark", choseDark);
+        Coffee selection = new Coffee(roast, "Cool Beans");
+        Coffee selection2 = new Coffee(roast, "Yummy Dummys");
+
+        selection.setRoast(roast);
         if (roast.equals("dark")) {
-            choseDark = true;
+            selection.setOrigin("Columbia");
+            selection.setOrigin("Brazil");
+
+        } else if (roast.equals("medium")) {
+            selection.setOrigin("New Guinea");
+            selection.setOrigin("Sumatra");
+
+        } else {
+            selection.setOrigin("Kenya");
+            selection.setOrigin("Ethiopa");
+
         }
-        model.addAttribute("choseDark", choseDark);
+        List<Coffee> selections = new ArrayList<>();
+        selections.add(selection);
+        selections.add(selection2);
+        model.addAttribute("roast", roast);
+
+        model.addAttribute("selections", selections);
 
         return "views-lec/coffee";
     }
